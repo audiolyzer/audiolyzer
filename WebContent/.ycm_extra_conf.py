3585ad3fcfd8194cfe4701043ea0da7bc55a1e95
niels <niels@AudiolyzerHQ>
2024-10-12 22:38:06 +0200

    a

--- WebContent/.ycm_extra_conf.py
+++
@@ -1,3 +0,0 @@
-def Settings(**kwargs):
-    if kwargs[ 'language' ] == 'cfamily':
-        return {'flags': ['-x', 'c++', '-std=c++17', '-Wall', '-Wconversion', '-Werror', '-Wno-sign-conversion', '-Wno-unknown-attributes']}
